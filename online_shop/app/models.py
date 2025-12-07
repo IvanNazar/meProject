e7f8c641dc138ca3498b06ad93235f0b9386d342
IvanNazar <atamnazar@gmail.com>
2025-12-07 10:01:38 +0100

    collumns and stuff

--- online_shop/app/models.py
+++ online_shop/app/models.py
@@ -5,7 +5,10 @@ db=SQLAlchemy()
 
 class Product(db.Model):
     __tablename__="products"
-    __tablename__="price"
     id=db.Column(db.Integer, primary_key=True, autoincrement=True )
     name=db.Column(db.String(100), nullable=False)
+    pricename=db.Column(db.Float, nullable=False)
 ##for all those collumns and rows
+created_at = db.Column(db.DateTime, default=db.func.now())
+updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+description =db.Column(db.Text, nullable=True)
--- online_shop/app/routes.py
+++ online_shop/app/routes.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template
+from .models import db, Product
 
-bp=Blueprint('routes', __name__)
+# Define Blueprint
+bp = Blueprint('routes', __name__)
 
+# Define route for the home page
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/products')
+def products():
+    products = Product.query.all()
+    return render_template('product_list.html', products=products)
