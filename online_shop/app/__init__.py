c1fa6473310d871a1467442eee2d7f9e18ef1e4a
IvanNazar <atamnazar@gmail.com>
2026-02-22 10:13:26 +0100

    server preparations

--- online_shop/app/__init__.py
+++ online_shop/app/__init__.py
@@ -13,12 +13,20 @@ def create_app():
         config = json.load(f)
     app.config.update(config)
     # Set up SQLAlchemy with absolute DB path
-    db_path = os.path.abspath(os.path.join(base_dir, config['DB_PATH']))
-    # Ensure the DB directory exists so SQLite can create the file
-    db_dir = os.path.dirname(db_path)
-    if not os.path.exists(db_dir):
-        os.makedirs(db_dir, exist_ok=True)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+  # Use DATABASE_URL env variable for Postgres if set, else fallback to SQLite
+    database_url = os.environ.get('DATABASE_URL')
+    db_path = None
+    if database_url:
+        print ("DB URL is set. Postgres DB")
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    else:
+        print ("DB URL is NOT set. SQLite DB")
+        db_path = os.path.abspath(os.path.join(base_dir, config['DB_PATH']))
+        db_dir = os.path.dirname(db_path)
+        if not os.path.exists(db_dir):
+            os.makedirs(db_dir, exist_ok=True)
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from .models import db
     db.init_app(app)
